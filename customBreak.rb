fddfcdf1420ee8d6dc15fcdeae51ba4ed76c1bbd
Revan <65150515+FerretUDP@users.noreply.github.com>
2024-09-10 17:35:36 +0200

    added in custom timing method for pomodoro timer under 'customBreak'

--- customBreak.rb
+++ customBreak.rb
@@ -5,7 +5,7 @@ def pomodoro_time(minutes)
 while seconds > 0
   minutes_left = seconds / 60
   seconds_left = seconds % 60
-# Display the time left in mm:ss format
+# Displays the time left in mm:ss format
   print "\r#{format('%02d', minutes_left)}:#{format('%02d', seconds_left)}"
 sleep(1)
   seconds -= 1
@@ -28,28 +28,37 @@ while seconds > 0
   sleep(1)
   seconds -= 1
 end
-puts "\nTake give, chefs!."
+  puts "\nTake give, chefs!."
 
 end
 
-
+#allows user to input custom timings
 #the user flow
-def start_timer
+def countdown_timer
   puts "welcome to tracking time through tomatoes!"
-loop do
+
+  loop do
+#user enters custom time
+  puts "Enter desired tomato session duration (minutes only)"
+    session_minutes = gets.chomp.to_i
+
+    puts "Enter duration of your break (minutes only)"
+    session_break = gets.chomp.to_i
+
+ #user starts the programme here
   puts "type in 'start' or 'exit' to begin/end your session"
-    input = gets.chomp.downcase
-  if input == 'start'
-    pomodoro_time(25)
-      takeFive(5)
+input = gets.chomp.downcase
+
+if input == 'start'
+    pomodoro_time(session_minutes)
+    takeFive(session_break)
   elsif input == 'exit'
-    puts "Cheers for now!"
-    break
+    puts "There will be no tomatoes today, sir! "
+  break
   else
-    puts "Error, input not found. Please enter 'start'/'exit', or fekoff"
+    puts "Error, input not found. Please enter 'start'/'exit, or-"
+    end
   end
 end
-end
 
-#calls and start the timer
-start_timer
+countdown_timer
